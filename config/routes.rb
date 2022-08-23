10dc2fb3af40ff5a3b0fcf164b10c2acebdeab04
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-08-23 17:32:05 +0200

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "tasks", to: "tasks#index", as: :index
-  get "tasks/new", to: "tasks#new", as: :new
+  get "tasks", to: "tasks#index", as: :tasks
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
   post "tasks", to: "tasks#create"
-  get "tasks/:id", to: "tasks#find", as: :task
+  get "tasks/new", to: "tasks#new", as: :new
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
